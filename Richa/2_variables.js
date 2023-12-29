503a91947594a6283e01123ac198ed56fde0c70c
Richa Chaudhary <66511611+r-icha@users.noreply.github.com>
2023-12-29 05:33:13 +0000

    variables final

---
+++ Richa/2_variables.js
@@ -0,0 +1,14 @@
+const accountId = 123432
+let ccountEmail = "richamaarula7@gmail.com"
+var accountpassword  = "12342234"
+accountCity="Hathras"// variable hm aise bhi declare krskte  but it not a good habit
+let accountState; // ; yeh use bhi krskte h aur nhi bhi and hm ek variable define krre  aur me koi value assign ki krri hu toh yeh mujhe output m (UNDIFINED) dega
+//accountId = 2
+console.log(accountId)
+accountEmail = "richacsjm.com"
+accountPassword = "234132"
+accountCity = "Jogia"
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+/*
+Prefer not to be use var because of issue in block scope and funcional scope
+*/
