1ad30de00f5af6000e49490bf449abe2f078a893
Richa Chaudhary <66511611+r-icha@users.noreply.github.com>
2024-01-08 13:33:23 +0000

    DataTypes

---
+++ Richa/03_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as new version of js
+
+// alert("hello")//we are using nodejs, not browser so yeh yaha work ni krega
+console.log("Richa") // code readability should be high
+
+let name = "Richa"
+let age = 18
+let isLoggedIn = false
+// data types:---
+//number range => 2 to the power of 53 tk ki hi value leskte h usse jyada ni
+//bigint :- this data type used in making big wedsites like stockmarket and youbute etc
+//string => ""
+//boolean => true/false
+//null => standalone value , it is representation of empty value isme hm intentionally bolte h ki wo undifined ni h wo khali h
+// undefined => it means abhi value assign ni hui h
+// symbol => unique
+
+// object
+console.log(typeof "Richa");
+console.log(typeof age);
+console.log(typeof null); // in output it gives object => it is important interview question
+console.log(typeof undefined); // in output it gives undefined. => it is important interview Question
