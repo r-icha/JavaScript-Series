2b63e9b4d48b76f307df9f5bd5fc863c63b596f0
Richa Chaudhary <66511611+r-icha@users.noreply.github.com>
2024-05-17 14:45:52 +0000

    strings

--- Richa/05_strings.js
+++ Richa/05_strings.js
@@ -7,7 +7,7 @@ console.log(name + repoCount + " value"); // this is the old syntax
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); 
 
 //declaration of sting aise bhi ho skta h
-const gameName = new String('richac')
+const gameName = new String('rich-ac')
 console.log(gameName);
 console.log(gameName[0]);
 console.log(gameName.length);
@@ -23,4 +23,16 @@ console.log(anotherString);
 
 const newStringOne = "  richa "
 console.log(newStringOne);
-console.log(newStringOne.trim()); // trim ko hm use krte h starting and ending space ko remve krne ke liye
+console.log(newStringOne.trim()); // trim ko hm use krte h starting and ending space ko remove krne ke liye
+
+const url = "https://richa.com/richa%20chaudhary"
+console.log(url.replace('%20', '-'));// here we replace %20 from - then we use this replace('value that we want to change', 'new value') string method
+
+console.log(url.includes('richa'));// if we want to know that is this word or something is present on our url or notthen we use includes() string method
+
+console.log(url.includes('sham'));
+
+console.log(gameName.split('-'));// yeh hmari string ko wahan se split kregi yahn yeh - symbol hoga aur ek array ke form m output degi
+
+
+
