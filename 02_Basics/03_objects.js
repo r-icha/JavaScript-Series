039e7d40971e8d51663d7cbe0a6ad8c42cc20a1b
Richa Chaudhary <66511611+r-icha@users.noreply.github.com>
2024-05-25 10:19:10 +0000

    objects

--- 02_Basics/03_objects.js
+++ 02_Basics/03_objects.js
@@ -5,8 +5,12 @@ Object.create // aise bhi object create krskte h is tarike ko hm bolte h constru
 
 //----const JsUser ={}-----
 //aise hm object bnate hain {}yeh curly braces hi object h aur yahan object ke andar keys aur value ka chhakar hota h
+
+const mySym = Symbol("key1")
+
 const JsUser ={
- name:"Richa",
+ name:"Richa",//by default system name ko string ki trh hi process krta h "name" aise but hm likhte  name aise hain kyunki system itna understand krleta h
+ mySym: "myKey1",
  age:18,
  location: "Hathras",
  email: "richa@gmail.com",
@@ -19,3 +23,5 @@ console.log(JsUser.email)// dot ki trh dete h toh isko string ki trh excess lene
 console.log(JsUser["email"])// isko hm
 //console.log(JsUser.full name); isko hm kbhi bhi dot lga ke excess ni krskte h iske liye square notation hi use krenge
 console.log(JsUser["full name"]);// full name ke liye aise hoga 
+console.log(JsUser.mySym);
+console.log(typeof JsUser.mySym);// its data type is string but we initialize mySym as symbol so this is the problem
