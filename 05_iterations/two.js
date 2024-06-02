c34e32192c311dd08fb2a7400d4cee353afb1f78
Richa Chaudhary <66511611+r-icha@users.noreply.github.com>
2024-06-02 18:37:12 +0000

    do and do-while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,35 @@
+let index = 0
+while(index <= 10){
+    console.log(`value of index is ${index}`);
+    index = index + 2
+}
+
+let myArray = ['flash', "batman", "superman"]
+let arr = 0
+while(arr < myArray.length){
+    console.log(`value is ${myArray[arr]}`);
+    arr = arr+1
+}
+
+
+// do-while loop
+/* -----syntax-----
+do{
+
+}while(condition);
+
+is loop m phle kaam hoga bd m condition check hogi*/
+
+let score = 1
+do{
+    console.log(`Score is ${score}`);
+    score++
+}while(score<=5)
+
+//----------------
+let score1 = 11
+do{
+    console.log(`Score is ${score1}`);
+    score1++
+}while(score1<=5) // iss loop m output 11 dega kyuki ek bar toh yeh chalega hi phir condition check hogi
+
